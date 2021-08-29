cd00bab10770d0708a9d7b7602bce23835735baf
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-08-29 16:11:15 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusText = new System.Windows.Forms.Label();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(198, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 72);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "〇✕ゲーム";
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Font = new System.Drawing.Font("Yu Gothic UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusText.Location = new System.Drawing.Point(147, 473);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(332, 72);
+            this.statusText.TabIndex = 3;
+            this.statusText.Text = "現在の手番 : ";
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn0.Location = new System.Drawing.Point(147, 117);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(109, 107);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "O";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(262, 117);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(109, 107);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "O";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(377, 117);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(109, 107);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "O";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(147, 230);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(109, 107);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "O";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(262, 230);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(109, 107);
+            this.btn4.TabIndex = 7;
+            this.btn4.Text = "O";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(377, 230);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(109, 107);
+            this.btn5.TabIndex = 8;
+            this.btn5.Text = "O";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(147, 343);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(109, 107);
+            this.btn6.TabIndex = 9;
+            this.btn6.Text = "O";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(262, 343);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(109, 107);
+            this.btn7.TabIndex = 10;
+            this.btn7.Text = "O";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("游ゴシック Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(377, 343);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(109, 107);
+            this.btn8.TabIndex = 11;
+            this.btn8.Text = "O";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.restartButton.Location = new System.Drawing.Point(211, 573);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(215, 51);
+            this.restartButton.TabIndex = 12;
+            this.restartButton.Text = "最初からはじめる";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 636);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusText;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button restartButton;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // "O", "X", ""のいずれか
+        private string[] board = new string[9];
+        private Boolean isPlayerX;
+        private int turnsCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = "";
+            }
+            drawButtons();
+            turnsCount = 0;
+            isPlayerX = true;
+            statusText.Text = "現在の手番 : " + (isPlayerX ? "X" : "O");
+        }
+        private void drawButtons() {
+            Button btn;
+            for (int i = 0; i < board.Length; i++)
+            {
+                string buttonName = "btn" + i.ToString();
+                btn = (Button)Controls.Find(buttonName, true)[0];
+
+                btn.Text = board[i];
+            }
+        }
+
+        private string CalculateWinner(string[] board)
+        {
+            int[,] lines = new int[8,3] 
+                {
+                    { 0, 1, 2},
+                    { 3, 4, 5},
+                    { 6, 7, 8},
+                    { 0, 3, 6},
+                    { 1, 4, 7},
+                    { 2, 5, 8},
+                    { 0, 4, 8},
+                    { 2, 4, 6}
+                };
+
+            for(int i = 0; i < lines.GetLength(0); i++)
+            {
+                if(
+                    board[lines[i, 0]] != "" &&
+                    board[lines[i,0]] == board[lines[i,1]] &&
+                    board[lines[i,0]] == board[lines[i,2]]
+                )
+                {
+                    return board[lines[i, 0]];
+                }
+            }
+
+            return "";
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int btnIndex = int.Parse(btn.Name.Replace("btn", ""));
+
+            if (board[btnIndex] != "" || CalculateWinner(board) != "" || turnsCount == 9)
+            {
+                return;
+            }
+
+            turnsCount++;
+            board[btnIndex] = isPlayerX ? "X" : "O";
+            drawButtons();
+
+            isPlayerX = !isPlayerX;
+            string winner = CalculateWinner(board);
+            if(winner != "")
+            {
+                statusText.Text = winner + "の勝ち！！";
+                return;
+            }
+
+            if(turnsCount == 9)
+            {
+                statusText.Text = "引き分け！！";
+                return;
+            }
+
+            statusText.Text = "現在の手番 : " + (isPlayerX ? "X" : "O");
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+    }
+}
